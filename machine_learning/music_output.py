91b93f78fe14ebcec019ca82272b8084df358b69
Kais Jessa <kaisjessa@gmail.com>
2018-09-24 09:50:40 -0400

    Added processing, training and testing files
    Need to add midi_to_text

---
+++ machine_learning/music_output.py
@@ -0,0 +1,55 @@
+#imports
+import numpy as np
+import pandas as pd
+import random
+import keras.models
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.utils import np_utils
+import pickle, sys
+#from data_preprocessing import *
+
+with open('data/scarlatti_objects.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
+    X, _, y, _, chat_to_int, int_to_char, chars = pickle.load(f)
+
+text = (open("data/scarlatti_text.txt").read())#.lower()
+chars = sorted(list(set(text.split(" "))))
+
+#load model
+model = keras.models.load_model("data/final_model.h5")
+
+#sample_char = "" if len(sys.argv) < 2 else sys.argv[1]
+#take an array of inputs
+output_array = []
+
+def check_model():
+    #random starting point from training data for generation
+    int_train = X[random.randint(0, len(X))]
+    #convert starting point back to characters
+    chars_array = [int_to_char[n] for n in int_train]
+
+    #number of characters to generate
+    for i in range(400):
+        #reshape data to feed to NN
+        x = np.reshape(int_train, (1, len(int_train), 1))
+        #normalize for NN
+        x = x / float(len(chars))
+
+        #the prediction is the index of the next character index
+        #argmax takes the highest number in the onehot array
+        int_prediction = np.argmax(model.predict(x, verbose=0))
+        #print(model.predict(x, verbose=0))
+
+        #append prediction to string array for output
+        chars_array.append(int_to_char[int_prediction])
+        output_array.append(int_to_char[int_prediction])
+
+        #append index to index array
+        int_train.append(int_prediction)
+        #drop first element for next iteration
+        int_train = int_train[1:]
+
+    predicted_text = "".join(output_array)
+    return(predicted_text)
+print(check_model())
---
+++ machine_learning/training.py
@@ -0,0 +1,46 @@
+#imports
+import numpy as np
+from keras.callbacks import ModelCheckpoint
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.utils import np_utils
+#from data_processing import *
+
+with open('data/scarlatti_objects.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
+    _, X_2, _, y_2, _, _, _ = pickle.load(f)
+
+#Keras NN
+model = Sequential()
+#add long short term memory cell
+#output is 700 units (length), input_shape is (number_of_inputs, input_length), return full sequence
+model.add(LSTM(700, input_shape=(X_2.shape[1], X_2.shape[2]), return_sequences=True)) #layer 1
+#account for overfitting
+model.add(Dropout(0.2))
+
+#add another LSTM layer
+model.add(LSTM(700, return_sequences=True)) #layer 2
+#account for overfitting
+model.add(Dropout(0.2))
+
+#add another LSTM layer
+model.add(LSTM(700)) #layer 3
+#account for overfitting
+model.add(Dropout(0.2))
+
+#Fully connected (dense) output layer
+model.add(Dense(y_2.shape[1], activation='softmax')) #layer 4
+#minimize loss
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+print("Model compiled")
+
+filepath = "data/current_best_model.h5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
+
+#train the model
+model.fit(X_2, y_2, epochs=100, batch_size=50, callbacks=callbacks_list)
+#save the model
+model.save("data/final_model.h5")
+print("Training Completed!")
