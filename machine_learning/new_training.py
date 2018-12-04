5673194c357c9f209355f713f9af117281f7070f
Kais Jessa <kaisjessa@gmail.com>
2018-12-04 11:46:02 -0500

    Tensorboard

--- machine_learning/new_training.py
+++ machine_learning/new_training.py
@@ -2,12 +2,14 @@
 import numpy as np
 import pickle
 from keras.callbacks import ModelCheckpoint
+from keras.callbacks import TensorBoard
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Activation
 from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.utils import np_utils
+import matplotlib.pyplot as plt
 #from data_processing import *
 
 with open('data/models/processed_data.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
@@ -33,10 +35,31 @@ print("Model compiled")
 
 filepath = "data/models/new_current_model.h5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=2, save_best_only=True, mode='min')
-callbacks_list = [checkpoint]
+tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+#tensorboard --logdir=logs/
+callbacks_list = [checkpoint, tensorboard]
 
 #train the model
 history = model.fit(X_2, y_2, epochs=128, batch_size=64, callbacks=callbacks_list)
+
+# Plot training & validation accuracy values
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
 #save the model
 model.save("data/models/5_test_model.h5")
 print("Training Completed!")
